638bb1a522736d1981c0da6cd41f13a057e06969
Kirk <kirk.harrison96@gmail.com>
2019-04-18 14:46:11 -0600

    basic functionality added

--- public/app.js
+++ public/app.js
@@ -5,14 +5,77 @@ var app = new Vue({
       // UI flags
       chatOpen: false,
       newmessage: "",
+      socket: null,
       message: 'Hello Vue!',
       scores: {},
       messages: []
     },
+    created: function(){
+      console.log('Vuejs is ready')
+      this.connectSocket();
+      this.refreshScoresAndMessages();
+    },
     methods:{
+      
       addMessage: function(){
-        this.messages.push(this.newmessage);
-        this.messages.reverse();
-      }
+        let data = {
+          action: 'add',
+          resource: 'messages',
+          data: this.newmessage
+        }
+        
+        this.socket.send(JSON.stringify(data));
+        this.newmessage = "";
+      },
+      refreshScoresAndMessages: function(){
+        this.socket.onopen = () => {
+          // refresh scores on open page
+          let request = {
+              action: 'list',
+              resource: 'scores'
+          }
+          this.socket.send(JSON.stringify(request));
+          // refresh messages on open page
+          let request2 = {
+              action: 'list',
+              resource: 'messages'
+          }
+          this.socket.send(JSON.stringify(request2));
+        }
+      },
+      connectSocket: function () {
+        // connect via WebSocket
+        this.socket = new WebSocket('ws://localhost:8080');
+        
+        // this.socket.onopen = () => {
+        //   // refresh scores on open page
+        //   let request = {
+        //       action: 'list',
+        //       resource: 'scores'
+        //   }
+        //   this.socket.send(JSON.stringify(request));
+        //   // refresh messages on open page
+        //   let request2 = {
+        //       action: 'list',
+        //       resource: 'messages'
+        //   }
+        //   this.socket.send(JSON.stringify(request2));
+        // }
+        this.socket.onmessage = event => {
+          var received = event.data;
+          var data = JSON.parse(received);
+          console.log(data)
+          if (data.action === 'list' && data.resource === 'scores') {
+            this.scores = data.data;
+            console.log('updated Scores')
+          }
+          if (data.action === 'list' && data.resource === 'messages'){
+            this.messages = data.data;
+            console.log("Messages", this.messages);
+            this.messages.reverse();
+          }
+
+        };
+      },
     }
   })
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const WebSocket = require('ws');
 
 var app = express();
 app.set('port', (process.env.PORT || 8080));
@@ -24,24 +25,98 @@ console.log("DATE", date);
 let fetchURL = "https://www.balldontlie.io/api/v1/games?start_date=" + date +"&" + "end_date=" + date;
 
 let scoreData = {};
+let messageList = [];
+
+
 function getScores(){
     fetch(fetchURL).then(response =>{
         response.json().then(data => {
             scoreData = data;
            // console.log(scoreData);
-            scoreData.data.forEach(game =>{
-                console.log("GAME", game.home_team, game.visitor_team);
-            })
+            // scoreData.data.forEach(game =>{
+            //     //console.log("GAME", game.home_team, game.visitor_team);
+            // })
+            sendAllScoresToAllSockets();
         });
     });
 }
 getScores();
 // Gets scores every 60 seconds
+
 setInterval(function() {  
-    let grabScores = getScores();    
-}, 60000);
+    let grabScores = getScores();
+    console.log('updated');
+}, 30000);
 
-
-app.listen(app.get('port'), function(){
+var server = app.listen(app.get('port'), function(){
     console.log("Server is listening...")
+});
+
+// WebSocket stuff below
+
+const wss = new WebSocket.Server({ server: server });
+
+//Use for messages and scores
+var broadcastToAllSockets = function (data) {
+  wss.clients.forEach(function (client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+};
+
+var sendAllMessagesToAllSockets = function () {
+    let data = {
+        resource: 'messages',
+        action: 'list',
+        data: messageList
+    };
+    broadcastToAllSockets(data);
+};
+var sendAllScoresToAllSockets = function () {
+    let data = {
+        resource: 'scores',
+        action: 'list',
+        data: scoreData
+    };
+    broadcastToAllSockets(data);
+    console.log("Updated Scores")
+};
+
+// wss.onopen = () => {
+//     ws.send(JSON.stringify(scoreData));
+//   }
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    data = JSON.parse(data);
+    console.log("data received from client:", data);
+    // REQUEST TO GET SCORES
+    if (data.action == "list" && data.resource == "scores") {
+          let data = {
+              resource: 'scores',
+              action: 'list',
+              data: scoreData
+          };
+          ws.send(JSON.stringify(data));
+      }
+    // REQUEST TO GET MESSAGES
+    else if (data.action == "list" && data.resource == "messages") {
+        //console.log('check here')
+        let data = {
+            resource: 'messages',
+            action: 'list',
+            data: messageList
+        };
+        ws.send(JSON.stringify(data));
+        console.log('asdas')
+    } 
+    // ADDS A MESSAGE
+    else if (data.action == "add" && data.resource == "messages"){
+        console.log('New message received', data);
+        messageList.push(data.data);
+        sendAllMessagesToAllSockets();
+
+    }
+  });
 });
