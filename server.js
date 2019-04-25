5e99d903fdc213871206d9919385a0031a997987
Kirk <kirk.harrison96@gmail.com>
2019-04-24 19:17:28 -0600

    Adjusted server to fetch every 60 seconds from api

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ getScores();
 setInterval(function() {  
     let grabScores = getScores();
     console.log('updated');
-}, 30000);
+}, 60000);
 
 var server = app.listen(app.get('port'), function(){
     console.log("Server is listening...")
